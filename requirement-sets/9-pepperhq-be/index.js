9590f809f73af632973a3cc248f48a70478ce646
Alexander Lind <alex.anonym@gmail.com>
2023-02-22 15:08:57 +0100

    finish backend challenge

--- requirement-sets/9-pepperhq-be/index.js
+++ requirement-sets/9-pepperhq-be/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import apicache from 'apicache';
 import router from './src/routers/menu.js';
 const app = express();
 const port = 3000;
+let cache = apicache.middleware;
 
+app.use(cache('24 hours')); // Cache all routes for 24 hours
 app.use('/', router)
 
 app.use((error, req, res, next) => {
@@ -12,5 +15,5 @@ app.use((error, req, res, next) => {
     res.status(500).send(error.message);
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
--- requirement-sets/9-pepperhq-be/src/routers/menu.js
+++ requirement-sets/9-pepperhq-be/src/routers/menu.js
@@ -4,16 +4,31 @@ import titles from "../../titles.json" assert { type: "json" };
 
 const router = Router();
 
-async function getMenu() {
+async function getMenu() { // Fetches menu from http://backend-challenge-pos.pepperhq.com/menu.json and returns it
     const response = await fetch('http://backend-challenge-pos.pepperhq.com/menu.json');
     const data = await response.json();
     return data.categories;
 }
 
+// Create a 24 hour cache for the menu
+
+
 router.get("/", async (req, res) => {
     const menu = await getMenu();
     console.log(titles, menu)
-    res.send(menu);
+    // Map through menu and look at each product.id and see if it matches any of the titles.id, if it does add the title to the product
+    menu.map((category) => {
+        category.products.map((product) => {
+            titles.map((title) => {
+                if (product.id === title.id) {
+                    product.title = title.title;
+                }
+            })
+        })
+    })
+    console.log(menu)
+
+    res.status(200).send(menu);
 });
 
 export default router;
